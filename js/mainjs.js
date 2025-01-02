f727ebd773ff1c10c25c066346e4956676cf2201
Bethireddi Lavanya <121751696+scetar@users.noreply.github.com>
2025-01-02 18:21:43 +0530

    Add files via upload

---
+++ js/mainjs.js
@@ -0,0 +1,12 @@
+$( "#bar_png" )
+    .filter( ":odd" )
+    .hide()
+    .end()
+    .filter( ":even" )
+    .hover(function() {
+        $( this )
+            .toggleClass( "active" )
+            .next()
+            .stop( true, true )
+            .slideToggle();
+    });
